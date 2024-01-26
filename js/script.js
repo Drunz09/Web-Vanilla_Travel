b055b70dbf9e5a22f34a878831aa58bb638c0a05
Drunz <mahludin09062000@gmail.com>
2024-01-26 10:50:09 +0700

    add features

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
+// Toggle Menu Opem
 const menuToggle = document.querySelector(".menu-toggle");
 const spanToggle = document.querySelectorAll(".menu-toggle span");
 const menuLink = document.querySelector(".menu ul");
-const searchMenu = document.querySelector(".search img");
-const showSearch = document.querySelector(".search input");
-const nav = document.querySelector("nav");
+const menuLi = document.querySelectorAll(".menu ul li a");
 
-// Toggle Menu
 menuToggle.addEventListener("click", () => {
-  const spanArray = [...spanToggle];
-
-  spanArray.map((e) => {
+  menuLink.classList.toggle("active");
+  spanToggle.forEach((e) => {
     return e.classList.toggle("active");
   });
-  menuLink.classList.toggle("active");
+
+  menuLi.forEach((e) =>
+    e.addEventListener("click", () => {
+      menuLink.classList.remove("active");
+      spanToggle.forEach((e) => {
+        return e.classList.remove("active");
+      });
+    })
+  );
 });
 
 // Search Menu
+const searchMenu = document.querySelector(".search img");
+const showSearch = document.querySelector(".search input");
+
 searchMenu.addEventListener("click", () => {
   showSearch.classList.toggle("active");
 });
+
+// NavChange
+const nav = document.querySelector("nav");
+const header = document.querySelector("header");
+
+window.onscroll = () => {
+  let headerHeight = header.scrollHeight;
+  if (window.scrollY > headerHeight - 100) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+};
